0201fc8e36c8e4b50b3e5320c096046d509c9491
dharani17chintha <dharanichintha6@gmail.com>
2026-08-31 14:41:35 +0530

    Fix production CORS origin

--- config/settings.py
+++ config/settings.py
@@ -138,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://vmc-hmi-frontend.onrender.com/",
+    "https://vmc-hmi-frontend.onrender.com",
 ]
